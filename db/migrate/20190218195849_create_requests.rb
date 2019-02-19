7eca4371f3ea972dda4b86eefdde5fe88c187ee4
Gary Franks <gary.udle@gmail.com>
2019-02-19 16:08:31 -0500

    changed request status default

--- db/migrate/20190218195849_create_requests.rb
+++ db/migrate/20190218195849_create_requests.rb
@@ -3,7 +3,7 @@ class CreateRequests < ActiveRecord::Migration[5.2]
     create_table :requests do |t|
       t.references :user, foreign_key: true
       t.references :item, foreign_key: true
-      t.string :status, default: "available"
+      t.string :status, default: "0"
 
       t.timestamps
     end
